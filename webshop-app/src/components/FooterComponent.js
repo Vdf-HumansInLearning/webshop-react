5dbb9182fc915f495b937bde58edd1665730d5df
Andreea Pantu <andreea.pantu@vodafone.com>
2022-03-07 11:11:14 +0200

    home page added

--- webshop-app/src/components/FooterComponent.js
+++ webshop-app/src/components/FooterComponent.js
@@ -8,10 +8,10 @@ function FooterComponent() {
         <div className="footer">
             <p>© 2021 Webshop | Follow us on</p>
             <ul className="footer-list">
-                <li className="footer-item"><a href="https://facebook.com/vodafonero" target="_blank"><FaFacebookSquare/></a></li>
-                <li className="footer-item"><a href="https://twitter.com/vodafonebuzz" target="_blank"><FaTwitterSquare/></a></li>
-                <li className="footer-item"><a href="https://www.linkedin.com/company/vodafone" target="_blank"><FaLinkedin/></a></li>
-                <li className="footer-item"><a href="https://instagram.com/vodafone.romania" target="_blank"><FaInstagramSquare/></a></li>
+                <li className="footer-item"><a href="https://facebook.com/vodafonero" target="_blank" rel="noreferrer"><FaFacebookSquare/></a></li>
+                <li className="footer-item"><a href="https://twitter.com/vodafonebuzz" target="_blank" rel="noreferrer"><FaTwitterSquare/></a></li>
+                <li className="footer-item"><a href="https://www.linkedin.com/company/vodafone" target="_blank" rel="noreferrer"><FaLinkedin/></a></li>
+                <li className="footer-item"><a href="https://instagram.com/vodafone.romania" target="_blank" rel="noreferrer"><FaInstagramSquare/></a></li>
             </ul>
         </div>
     );
