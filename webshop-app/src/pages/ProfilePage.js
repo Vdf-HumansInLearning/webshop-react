18d69b6730a1b6ca790a8cbd02f45623b8fca74d
Andreea Pantu <andreea.pantu@vodafone.com>
2022-03-10 18:02:22 +0200

    fix for address on Profile Page

--- webshop-app/src/pages/ProfilePage.js
+++ webshop-app/src/pages/ProfilePage.js
@@ -1,22 +1,30 @@
 import FooterComponent from "../components/FooterComponent";
 import NavbarComponent from "../components/NavbarComponent";
 import EditProfileModal from "../components/EditProfileModal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 
 function ProfilePage() {
   const [modalShow, setModalShow] = useState(false);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
 
   let logged_in = false;
   let user_id = localStorage.getItem("user_id");
+
   if (user_id) {
     logged_in = true;
-    fetch("http://localhost:3001/users/" + user_id)
-      .then((response) => response.json())
-      .then((data) => setUser(data));
   }
-  console.log(typeof user.address);
+  
+  useEffect(() => {
+    if (user_id && logged_in) {
+      fetch("http://localhost:3001/users/" + user_id)
+        .then((response) => response.json())
+        .then((data) => {
+          setUser(data)});
+    }
+  }, [])
+  
+  
   return (
     <>
       <NavbarComponent />
@@ -24,12 +32,7 @@ function ProfilePage() {
         className="container d-flex flex-column align-items-center mt-5 pt-5"
         id="container"
       >
-        {logged_in === false ? (
-          <>
-            <h3 className="main-title">You are not logged in.</h3>
-            <a href="/auth/login">Go to Login page</a>
-          </>
-        ) : (
+        {logged_in && user ? (
           <>
             <h3 className="main-title">
               <u>Your Profile</u>
@@ -43,7 +46,7 @@ function ProfilePage() {
                 <strong>Email: </strong>
               </dt>
               <dd>{user.email}</dd>
-              {/* {user.address.street ? (
+              {user.address.street ? (
                 <>
                   <dt>
                     <strong>Address: </strong>
@@ -75,12 +78,17 @@ function ProfilePage() {
                   </dt>
                   <dd>You didn't saved a phone number yet.</dd>
                 </>
-              )} */}
+              )}
             </dl>
 
             <Button variant="outline-danger" onClick={() => setModalShow(true)}>
               Edit Profile
             </Button>
+          </>
+        ) : (
+          <>
+            <h3 className="main-title">You are not logged in.</h3>
+            <a href="/auth/login">Go to Login page</a>
           </>
         )}
       </div>
