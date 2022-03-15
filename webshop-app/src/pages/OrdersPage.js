018c6287d093bb9a9e3c7abeb307f89cafc6f5e3
Ionela <ionela.ciobanasu@vodafone.com>
2022-03-15 11:16:03 +0200

    started working on order history page

--- webshop-app/src/pages/OrdersPage.js
+++ webshop-app/src/pages/OrdersPage.js
@@ -1,14 +1,41 @@
 import FooterComponent from "../components/FooterComponent";
 import NavbarComponent from "../components/NavbarComponent";
+import Order from "../components/Order";
+import "../css/Orders.css";
+import React from "react";
+import { useState, useEffect } from "react";
 
 function OrdersPage() {
-    return (
-        <>
-            <NavbarComponent/>
-            <div>Orders Page</div>
-            <FooterComponent/>
-        </>
-    );
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/orders")
+      .then((response) => response.json())
+      .then((data) => setOrders(data));
+  }, []);
+
+  console.log(orders);
+  return (
+    <>
+      <NavbarComponent />
+      <main className="main-main">
+        {orders.map((order) => (
+          <Order
+            key={order.id}
+            id={order.id}
+            email={order.email}
+            name={order.name}
+            phone={order.name}
+            date={order.date}
+            price={order.order[0].price}
+            quantity={order.order[0].quantity}
+            deliveryAddress={order.delivery_address}
+          />
+        ))}
+      </main>
+      <FooterComponent />
+    </>
+  );
 }
 
 export default OrdersPage;
