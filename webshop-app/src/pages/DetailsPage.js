6231b84b38d1bb1e3a18dec5239bbdb83f823688
stefanbcs <stefan.bcs10@gmail.com>
2022-03-16 03:48:38 +0200

    phone details page

--- webshop-app/src/pages/DetailsPage.js
+++ webshop-app/src/pages/DetailsPage.js
@@ -1,11 +1,132 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import FooterComponent from "../components/FooterComponent";
 import NavbarComponent from "../components/NavbarComponent";
 
 function DetailsPage() {
+  const [phone, setPhone] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/phones/" + window.location.pathname.slice(-1))
+      .then(function (response) {
+        setPhone(response.data);
+      });
+  }, []);
+
+  function getRating(rating) {
+    let elements = ["Rating: "];
+
+    for (let i = 1; i <= 5; i++) {
+      Math.trunc(rating) >= i
+        ? elements.push(
+            React.createElement("span", { className: "fa fa-star checked" })
+          )
+        : // <span className="fa fa-star checked"></span>
+          elements.push(
+            React.createElement("span", { className: "fa fa-star unchecked" })
+          );
+      // <span className="fa fa-star unchecked"></span>
+    }
+
+    rating > 0
+      ? elements.push(React.createElement("span", rating))
+      : elements.push(React.createElement("span", "(-)"));
+
+    let starRating = React.createElement("h5", elements);
+    return starRating;
+  }
+
+  console.log(phone.image);
+
   return (
     <>
       <NavbarComponent />
-      <div>Phone Page</div>
+      {phone ? (
+        <div className="container pt-5" id="container">
+          <div className="title mt-3">
+            <h4 className="title" id="brand">
+              {phone.brand}{" "}
+              <span className="subtitle text-danger" id="name">
+                {phone.name}
+              </span>
+            </h4>
+          </div>
+          <div className="container mt-3">
+            <div className="row">
+              <div className="phone-img col-sm-6 col-12" id="phone-img">
+                <img
+                  src={`../../public/images/${phone.image}`}
+                  alt="Phone placeholder"
+                ></img>
+              </div>
+              <div
+                className="details col-sm-6 col-12 align-self-center"
+                id="phone-details"
+              >
+                {phone.discount > 0 ? (
+                  <h5>
+                    Price : <span className="discounted">{phone.price}</span>{" "}
+                    <span id="price">{phone.price - phone.discount}</span> RON
+                  </h5>
+                ) : (
+                  <h5>
+                    Price : <span id="price">{phone.price}</span> RON
+                  </h5>
+                )}
+                <div id="rating">
+                  {/* <h5>
+                   Rating :{}
+                   {phone.rating > 0 ? (
+                     <span> (phone.rating)</span>
+                   ) : (
+                     <span> (-)</span>
+                   )}
+                </h5> */}
+                </div>
+                <p>(Average: average </p>
+                <h5>
+                  Operating system :{" "}
+                  <span className="detail-value">{phone.operating_system}</span>
+                </h5>
+                <h5>
+                  Available from date :{" "}
+                  <span className="detail-value">
+                    {phone.availability_date}
+                  </span>
+                </h5>
+                <h5>
+                  In stock :
+                  {phone.quantity > 0 ? (
+                    <span className="detail-value"> YES</span>
+                  ) : (
+                    <span className="detail-value"> NO</span>
+                  )}
+                </h5>
+                {phone.quantity > 0 ? (
+                  <button
+                    id="add-to-cart"
+                    className="text-center btn btn-outline-danger mt-3"
+                  >
+                    Add to cart
+                  </button>
+                ) : (
+                  <button
+                    id="add-to-cart"
+                    className="text-center btn btn-outline-danger mt-3"
+                    disabled
+                  >
+                    Add to cart
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
       <FooterComponent />
     </>
   );
