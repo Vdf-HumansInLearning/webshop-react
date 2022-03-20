ff63bc0b06ee1ee0f2b71e2adfde40f11f90480a
stefanbcs <stefan.bcs10@gmail.com>
2022-03-20 16:09:28 +0200

    delete phone functionality

--- webshop-app/src/components/Phone.js
+++ webshop-app/src/components/Phone.js
@@ -1,4 +1,7 @@
 import React from "react";
+import EditPhoneModal from "./EditPhoneModal";
+import DeletePhoneModal from "./DeletePhoneModal";
+import { useState } from "react";
 
 function Phone({
   id,
@@ -12,61 +15,84 @@ function Phone({
   discount,
   date,
 }) {
-  return (
-    <div className="phone-card col-lg-3 col-md-4 col-sm-6 col-12 g-3">
-      <div
-        className="action-buttons d-flex justify-content-end"
-        id={id}
-      >
-        <a href="/phones/<%= products[i].id %>/edit">
-          <button className="edit-btn">Edit</button>
-        </a>
-        <button
-          className="delete-btn"
-          data-bs-toggle="modal"
-          data-bs-target="#confirm-delete"
-        >
-          Delete
-        </button>
-      </div>
-      <div className="phone-img d-flex justify-content-center align-items-center">
-        <img src={"images/" + image} alt="Phone placeholder" />
-      </div>
-      <div className="phone-info">
-        <h3 className="title">{brand}</h3>
-        <h4 className="subtitle">{name}</h4>
-        <p>from</p>
-        {discount > 0 ? (
-          <h5>
-            Price : <span className="discounted">{price}</span>
-            <span className="price">{price - discount}</span>RON
-          </h5>
-        ) : (
-          <h5>
-            Price : <span className="price">{price}</span>RON
-          </h5>
-        )}
+  const [editModalShow, setEditModalShow] = useState(false);
+  const [deleteModalShow, setDeleteModalShow] = useState(false);
 
-        <a
-          href={'/phones/' + id}
-          className="phone-link mb-2"
-        >
-          <button className="btn btn-danger">See product details &gt;</button>
-        </a>
-        {quantity > 0 ? (
-          <button className="add-to-cart text-center btn btn-danger mt-2">
-            Add to cart
+  const deletePhone = () => {
+    console.log(id);
+    fetch("http://localhost:3001/phones/" + id, {
+      method: "DELETE",
+    });
+  };
+  return (
+    <>
+      <DeletePhoneModal
+        deletePhone={deletePhone}
+        onHide={() => setDeleteModalShow(false)}
+        show={deleteModalShow}
+      />
+      <EditPhoneModal
+        brand={brand}
+        name={name}
+        os={os}
+        price={price}
+        discount={discount}
+        quantity={quantity}
+        date={date}
+        rating={rating}
+        show={editModalShow}
+        onHide={() => setEditModalShow(false)}
+      />
+      <div className="phone-card col-lg-3 col-md-4 col-sm-6 col-12 g-3">
+        <div className="action-buttons d-flex justify-content-end" id={id}>
+          <button className="edit-btn" onClick={() => setEditModalShow(true)}>
+            Edit
           </button>
-        ) : (
           <button
-            className="add-to-cart text-center btn btn-danger mt-2"
-            disabled
+            className="delete-btn"
+            data-bs-toggle="modal"
+            data-bs-target="#confirm-delete"
+            onClick={() => setDeleteModalShow(true)}
           >
-            Add to cart
+            Delete
           </button>
-        )}
+        </div>
+        <div className="phone-img d-flex justify-content-center align-items-center">
+          <img src={"images/" + image} alt="Phone placeholder" />
+        </div>
+        <div className="phone-info">
+          <h3 className="title">{brand}</h3>
+          <h4 className="subtitle">{name}</h4>
+          <p>from</p>
+          {discount > 0 ? (
+            <h5>
+              Price : <span className="discounted">{price}</span>
+              <span className="price">{price - discount}</span>RON
+            </h5>
+          ) : (
+            <h5>
+              Price : <span className="price">{price}</span>RON
+            </h5>
+          )}
+
+          <a href={"/phones/" + id} className="phone-link mb-2">
+            <button className="btn btn-danger">See product details &gt;</button>
+          </a>
+          {quantity > 0 ? (
+            <button className="add-to-cart text-center btn btn-danger mt-2">
+              Add to cart
+            </button>
+          ) : (
+            <button
+              className="add-to-cart text-center btn btn-danger mt-2"
+              disabled
+            >
+              Add to cart
+            </button>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
