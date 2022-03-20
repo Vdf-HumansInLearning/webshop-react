0aa9f38c384a792850c29b4e323b38add35cdcc4
stefanbcs <stefan.bcs10@gmail.com>
2022-03-20 16:14:42 +0200

    state vaiable and function renamed

--- webshop-app/src/pages/StorePage.js
+++ webshop-app/src/pages/StorePage.js
@@ -1,27 +1,28 @@
 import FooterComponent from "../components/FooterComponent";
 import NavbarComponent from "../components/NavbarComponent";
 import AddPhoneModal from "../components/AddPhoneModal";
-import EditPhoneModal from "../components/EditPhoneModal";
 import PhoneList from "../components/PhoneList";
 import { Button } from "react-bootstrap";
 import { useState } from "react";
 
-import '../css/StorePage.css';
+import "../css/StorePage.css";
 
 function StorePage() {
-  const [modalShow, setModalShow] = useState(false);
+  const [addModalShow, setAddModalShow] = useState(false);
 
   return (
     <>
       <NavbarComponent />
       <PhoneList />
       <div className="d-flex justify-content-center mb-5 mt-5">
-        <Button variant="danger" onClick={() => setModalShow(true)}>
-            Add Phone
+        <Button variant="danger" onClick={() => setAddModalShow(true)}>
+          Add Phone
         </Button>
       </div>
-      
-      <AddPhoneModal show={modalShow} onHide={() => setModalShow(false)} />
+      <AddPhoneModal
+        show={addModalShow}
+        onHide={() => setAddModalShow(false)}
+      />
       <FooterComponent />
     </>
   );
