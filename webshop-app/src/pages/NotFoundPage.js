339681e30c80379454f69087253aa4edac3aa40c
catalin tugui <catalin.tugui@vodafone.com>
2022-03-30 10:14:30 +0300

    page 404

--- webshop-app/src/pages/NotFoundPage.js
+++ webshop-app/src/pages/NotFoundPage.js
@@ -1,6 +1,14 @@
+import '../css/NotFoundPage.css';
+import {Link} from 'react-router-dom';
+
 function NotFoundPage() {
     return (
-       <div>404 NOT FOUND</div>
+       <div className='main-notfound'>
+           <div className='notfound-content'>
+                <h1 className='notfound-header'>Oups...something went wrong</h1>
+                <h2 className='notfound-link'>Go back to <Link className='link' to='/'>HOMEPAGE</Link></h2>
+           </div>
+        </div>
     );
 }
 
